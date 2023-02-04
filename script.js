c0c9d462b7ea5f934ec48640d6531c0238fcc125
SK-0911 <shubham.911k@gmail.com>
2023-02-04 12:37:33 +0530

    Basic static design

---
+++ script.js
@@ -0,0 +1,11 @@
+let num = 0;
+
+const value = document.querySelector(".value")
+
+const btns = document.querySelectorAll(".btn")
+
+// console.log(btns);
+
+btns.forEach((btn) => {
+    console.log(btn);
+})
